334fb1f17664bf72e9a41bc45cb78fcf0fbfdf5e
Felipe Mendoza <felipe188.mendoza@gmail.com>
2023-06-29 20:43:04 -0500

    agregar funcion para obtener el archivo file

--- src/component/CardProcess/CardProcess.tsx
+++ src/component/CardProcess/CardProcess.tsx
@@ -7,14 +7,14 @@ interface CardProcessProps{
     description: string;
     is_process: boolean;
     date: string;
-  }
-const CardProcess:React.FC<CardProcessProps> = ({title,description,is_process,date})=>{
+}
+const CardProcess:React.FC<CardProcessProps>=({title,description,is_process,date})=>{
     return (
         <div className="card">
             <h2 className="h2">{title}</h2>
             <p className="description">{description}</p>
             <p className="datetime">{date}</p>
-            {is_process ?(
+            { !is_process ? (
                 <div className="loader">
                     <div><FontAwesomeIcon icon={faCheckSquare} size="xl" color="green" /></div>
                     <div className="spinner"></div>
--- src/pages/Upload/UploadScreen.tsx
+++ src/pages/Upload/UploadScreen.tsx
@@ -1,23 +1,44 @@
 import Swal from 'sweetalert2';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import './css/upload.css';
+  
 const UploadScreen = ()=>{
+    const [state,setState] = useState<Number>(0);
+
     useEffect(()=>{
-        Swal.fire({
-            position:'bottom-right',
-            title: 'Error!',
-            icon: 'info',
-            heightAuto:false,
-            showConfirmButton:false
-        })
-    },[]);
+        if(state === 1){
+            Swal.fire({
+                position:'top-right',
+                title: 'Cargando',
+                icon: 'info',
+                showConfirmButton:false
+            })
+            return ;
+        }
+        if(state === 2){
+            Swal.fire({
+                position:'top-right',
+                title: 'Finalizado',
+                icon: 'success',
+                showConfirmButton:false
+            });
+        }
+        return ;
+    },[state]);
+    const ChangeFile=(valueFile:any)=>{
+        console.log(valueFile);
+        setState(1);
+        setTimeout(()=>{
+            setState(2);
+        },5000);
+    }
     return (
         <div className="dashboard-wrapper">
             <h1>Procesar documentos</h1>
-            <br />
-            <input type="file" name="file" id="idfile" />
-            <button>
-                hola mundo
-            </button>
+            <hr style={{marginTop:10,marginBottom:10}} />
+            <div className='file-select' id='src-file1'>
+                <input onChange={(e)=>ChangeFile(e.target.files)} type="file" name="file" id="dataExcel" />
+            </div>
         </div>
     );
 };
